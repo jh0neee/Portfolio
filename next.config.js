223e5f1e1f8411a1c182351d4fa9dbe624c1b3b9
지현 <jh0neee@naver.com>
2023-12-17 07:02:21 +0900

    chore:next.config.js 파일 업데이트

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
+};
