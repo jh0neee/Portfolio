ff2a0b9533b96d17744386e5768caa234dc262b2
지현 <jh0neee@naver.com>
2023-12-17 01:57:32 +0900

    style:global style 수정

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -3,12 +3,21 @@ import { normalize } from "styled-normalize";
 
 export const GlobalStyle = createGlobalStyle`
   ${normalize}
+  *{
+    margin: 0;
+    padding: 0;
+  }
   html {
     box-sizing: border-box;
     font-size: 16px;
     min-width: 375px;
   }
   a {
-    cursor: pointer; text-decoration: none;
+    cursor: pointer;
+    text-decoration: none;
+    color: inherit;
+  }
+  ul {
+    list-style: none;
   }
 `;
