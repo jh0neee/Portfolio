81524d44c8290c9467ccc8fcb63ae493093712dd
지현 <jh0neee@naver.com>
2023-12-24 15:24:52 +0900

    feat:react-query, recoil 설정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,21 @@
-import { GlobalStyle } from "@/styles/global-styles";
-import { theme } from "@/styles/theme";
+import React from "react";
 import { AppProps } from "next/app";
+import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
+import { theme } from "@/styles/theme";
+import { GlobalStyle } from "@/styles/global-styles";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(() => new QueryClient());
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <GlobalStyle />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
