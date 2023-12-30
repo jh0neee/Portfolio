5a3910d54c56d4972e11d2019678f696eee17ad6
지현 <jh0neee@naver.com>
2023-12-30 15:29:59 +0900

    feat:data 초기 상태 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import styled from "styled-components";
 import SideBar from "@/components/layout/SideBar";
 import NavBar from "@/components/layout/NavBar";
 import Project from "@/components/views/project/project";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+import { getData } from "@/firebase/firebase";
 
 const Home: NextPage = () => {
   return (
@@ -19,6 +21,21 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery({
+    queryKey: ["project"],
+    queryFn: () => getData("project"),
+  });
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
 
 export const ContentLayout = styled.div`
   height: calc(100% - 120px);
